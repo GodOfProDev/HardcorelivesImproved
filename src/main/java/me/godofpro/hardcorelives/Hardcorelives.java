4994767f780e70669d137df55fb1d714488c7782
GodOfPro <1387ilia@gmail.com>
2022-05-04 23:31:41 +0430

    Added vault api

--- src/main/java/me/godofpro/hardcorelives/Hardcorelives.java
+++ src/main/java/me/godofpro/hardcorelives/Hardcorelives.java
@@ -5,12 +5,21 @@ import me.godofpro.hardcorelives.commands.LiveCommand;
 import me.godofpro.hardcorelives.commands.MainCommand;
 import me.godofpro.hardcorelives.listeners.PlayerDeathListener;
 import me.godofpro.hardcorelives.listeners.PlayerJoinListener;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Hardcorelives extends JavaPlugin {
+    private static Economy econ = null;
 
     @Override
     public void onEnable() {
+        if(!setupEconomy())
+        {
+            getServer().getLogger().warning("Disaling the buy command because no vault");
+            // TODO make the buy command
+            // TODO disabled the buy command if there is no economy
+        }
         getServer().getPluginManager().registerEvents(new PlayerDeathListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
         registerCommands();
@@ -25,5 +34,17 @@ public final class Hardcorelives extends JavaPlugin {
         PaperCommandManager pcm = new PaperCommandManager(this);
         pcm.registerCommand(new LiveCommand(this));
         pcm.registerCommand(new MainCommand(this));
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
     }
 }
